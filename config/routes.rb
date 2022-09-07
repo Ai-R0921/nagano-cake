c31fef6450a2914146fcfff5e8835980a88fe820
Ai-R0921 <xoa1xo.0921@gmail.com>
2022-09-07 14:36:33 +0000

    [WIP]addressesテーブル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :admins, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
+  # devise_for :admins, skip: [:registrations, :passwords], controllers: {
+  #   sessions: "admin/sessions"
+  # }
+  # devise_for :customers, skip: [:passwords], controllers: {
+  #   registrations: "public/registrations"
+  #   sessions: 'public/sessions'
+  # }
   devise_for :customers, skip: [:passwords], controllers: {
-    registrations: "public/registrations"
-    sessions: 'public/sessions'
-    
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
