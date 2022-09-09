04d8f01086dd743de613af06b3982380d40bfe2a
Ai-R0921 <xoa1xo.0921@gmail.com>
2022-09-09 15:47:07 +0000

    [WIP]管理者側コントローラー作成(orders,order_details以外)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/create'
+    get 'genres/edit'
+    get 'genres/update'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/create'
+    get 'items/show'
+    get 'items/edit'
+    get 'items/update'
+  end
   # devise_for :admins, skip: [:registrations, :passwords], controllers: {
   #   sessions: "admin/sessions"
   # }
