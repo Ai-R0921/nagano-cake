2cb564150352d9e4e09f8647a7bdbb9aa1b74545
Ai-R0921 <xoa1xo.0921@gmail.com>
2022-09-14 14:27:55 +0000

    [WIP]ジャンル画面の機能完成、商品新規登録画面作成中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -8,9 +8,13 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item)
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
@@ -18,4 +22,11 @@ class Admin::ItemsController < ApplicationController
 
   def update
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :is_active, :genre)
+  end
+
+
 end
