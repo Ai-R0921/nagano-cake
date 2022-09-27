cd682bc5de4e9c83d5de514a963ec13671c6d319
Ai-R0921 <xoa1xo.0921@gmail.com>
2022-09-27 12:01:10 +0000

    [WIP]新規会員登録画面作成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,5 @@
 Rails.application.routes.draw do
 
-  scope module: 'public' do
-    resources :addresses, except: [:show, :new]
-    resources :orders, except: [:edit, :update, :destroy]
-    post 'orders/confirm'
-    get 'orders/complete'
-    resources :cart_items, except: [:new, :show, :edit]
-    delete 'cart_items/destroy_all'
-    resources :customers, only: [:show, :edit, :update]
-    get 'customers/unsubscribe'
-    patch 'customers/withdraw'
-    resources :items, only: [:index, :show]
-    root to: 'homes#top'
-    get '/about' => 'homes#about',as:"about"
-  end
-
-
-  namespace :admin do
-    patch 'order_details/update'
-    resources :orders, only: [:show, :update]
-    resources :customers, except: [:new, :create, :destroy]
-    resources :genres, except: [:new, :show, :destroy]
-    get '/' => 'homes#top', as: 'top'
-    resources :items, except: [:destroy]
-  end
-  # devise_for :admins, skip: [:registrations, :passwords], controllers: {
-  #   sessions: "admin/sessions"
-  # }
-  # devise_for :customers, skip: [:passwords], controllers: {
-  #   registrations: "public/registrations"
-  #   sessions: 'public/sessions'
-  # }
   devise_for :customers, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
@@ -42,5 +11,30 @@ Rails.application.routes.draw do
   sessions: "admin/sessions"
 }
 
+scope module: 'public' do
+    root to: 'homes#top'
+    get '/about' => 'homes#about',as:"about"
+    post 'orders/confirm'
+    get 'orders/complete'
+    delete 'cart_items/destroy_all'
+    get 'customers/unsubscribe'
+    patch 'customers/withdraw'
+    resources :addresses, except: [:show, :new]
+    resources :orders, except: [:edit, :update, :destroy]
+    resources :customers, only: [:show, :edit, :update]
+    resources :cart_items, except: [:new, :show, :edit]
+    resources :items, only: [:index, :show]
+  end
+
+
+
+namespace :admin do
+    get '/' => 'homes#top', as: 'top'
+    patch 'order_details/update'
+    resources :orders, only: [:show, :update]
+    resources :customers, except: [:new, :create, :destroy]
+    resources :genres, except: [:new, :show, :destroy]
+    resources :items, except: [:destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
