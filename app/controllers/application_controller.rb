d1ee84457dcd6e186eda25dd192bd394eceb378a
Ai-R0921 <xoa1xo.0921@gmail.com>
2022-09-29 14:40:57 +0000

    [WIP]新規会員登録画面、ログイン画面完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   protected
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
   end
 
   def after_sign_in_path_for(resource)
-    admin_top_path
+    case resource
+    when Customer
+      customer_path(current_customer)
+    when Admin
+      admin_top_path
+    end
   end
-  
+
 end
