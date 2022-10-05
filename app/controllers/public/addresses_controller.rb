ab20a65976501a36a6e0a732f644276c97bb8531
Ai-R0921 <xoa1xo.0921@gmail.com>
2022-10-05 14:31:06 +0000

    [WIP]各配送関連画面作成中

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,20 +1,30 @@
 class Public::AddressesController < ApplicationController
   def index
-    @address = current_customer
+    @address = Address.new
+    @addresses = Address.all
   end
 
   def edit
+    @address = current_customer
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.save
+    redirect_to addresses_path
   end
 
   def update
   end
 
   def destroy
+    @address = current_customer
+    @address.destroy
+    redirect_to addresses_path
   end
-
+  
+  
+  private
   def address_params
     params.require(:address).permit(:name, :postal_code, :address)
   end
