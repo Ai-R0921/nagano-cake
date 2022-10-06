4b2ec28fb1e5a1cc2ca46a27cf89a97491d81bfc
Ai-R0921 <xoa1xo.0921@gmail.com>
2022-10-06 14:20:26 +0000

    [Finish]配送関連画面完成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ItemsController < ApplicationController
   def index
-    @items = Item.page(params[:page])
+    @items = Item.page(params[:id])
   end
 
   def new
--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -5,25 +5,29 @@ class Public::AddressesController < ApplicationController
   end
 
   def edit
-    @address = current_customer
+    @address = Address.find(params[:id])
   end
 
   def create
     @address = Address.new(address_params)
-    @address.save
+    @address.customer_id = current_customer.id
+    @address.save!
     redirect_to addresses_path
   end
 
   def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path
   end
 
   def destroy
-    @address = current_customer
+    @address = Address.find(params[:id])
     @address.destroy
     redirect_to addresses_path
   end
-  
-  
+
+
   private
   def address_params
     params.require(:address).permit(:name, :postal_code, :address)
