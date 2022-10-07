69066be9fe278b568c8cb4a813841c046917f5c9
Ai-R0921 <xoa1xo.0921@gmail.com>
2022-10-07 15:08:36 +0000

    [WIP]会員側商品一覧画面作成中

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,8 +1,14 @@
 class Public::ItemsController < ApplicationController
   def index
-    
+    @items = Item.page(params[:page])
   end
 
   def show
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :price, :is_active)
   end
 end
