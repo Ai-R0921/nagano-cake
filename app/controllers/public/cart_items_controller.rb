f9cc27b0c65afd58f5e051a36328a7aa239ae318
Ai-R0921 <xoa1xo.0921@gmail.com>
2022-10-09 16:13:10 +0000

    [WIP]カート機能実装中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,7 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = current_customer.cart_items
+    @total = 0
   end
 
   def update
@@ -9,8 +11,21 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy_all
+    CartItem.destroy_all
+    current_customer.cart_item.destroy_all
+    redirect_to cart_items_path
   end
 
   def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item = current_customer.cart_items.build(cart_item_params)
+    @cart_item.save
   end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+  end
+
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -5,11 +5,18 @@ class Public::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item = current_customer.cart_items.build(cart_item_params)
+    @cart_item.save
   end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :introduction, :price, :is_active)
+  end
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end
