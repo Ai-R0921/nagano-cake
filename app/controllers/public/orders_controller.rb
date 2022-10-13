0a0b6728fdaf6165bb67a5dfd0f81b9a5d03f075
Ai-R0921 <xoa1xo.0921@gmail.com>
2022-10-13 15:11:31 +0000

    [WIP]注文情報入力画面作成中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,6 +4,28 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @order = Order.new(order_params)
+    if params[:order][:address_number] == "1"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.name
+    elsif params[:order][:address_number] == "2"
+      if Address.exists?(name: params[:order][:registered])
+        @order.postal_code = Address.find(params[:order][:registered]).postal_code
+        @order.address = Address.find(params[:order][:registered]).address
+        @order.name = Address.find(params[:order][:registered]).name
+      else
+        render :new
+      end
+    elsif params[:order][:address_number] == "3"
+      address_new = current_customer.Address.new(address_params)
+      if address_new.save
+      else
+        render :new
+      end
+      @cart_items = current_customer.cart_items.all
+      @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
+    end
   end
 
   def complete
@@ -16,5 +38,16 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+  end
+
+
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :postal_code, :address, :name, :payment_method, :total_payment, :postage, :status)
+  end
+
+  def address_params
+    params.require(:address).permit(:customer_id, :name, :postal_code, :address)
   end
 end
