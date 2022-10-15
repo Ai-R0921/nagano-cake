36ddd7e76ec32402523f50d53cc5a881ea1b8940
Ai-R0921 <xoa1xo.0921@gmail.com>
2022-10-15 16:57:05 +0000

    [WIP]注文情報入力画面完成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,7 +8,7 @@ class Public::OrdersController < ApplicationController
     if params[:order][:address_number] == "1"
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
-      @order.name = current_customer.name
+      @order.name = current_customer.full_name
     elsif params[:order][:address_number] == "2"
       if Address.exists?(name: params[:order][:registered])
         @order.postal_code = Address.find(params[:order][:registered]).postal_code
@@ -23,9 +23,11 @@ class Public::OrdersController < ApplicationController
       else
         render :new
       end
-      @cart_items = current_customer.cart_items.all
-      @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
     end
+    @cart_items = current_customer.cart_items.all
+    @order.postage = 800
+    @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
+    @order.total_payment = @order.postage + @total.round.to_i
   end
 
   def complete
