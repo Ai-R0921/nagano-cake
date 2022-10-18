af0f3b005e69d43f73983ee0e3e67ea366cbbc9e
Ai-R0921 <xoa1xo.0921@gmail.com>
2022-10-18 11:53:02 +0000

    [WIP]管理者側注文関連画面作成中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,18 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    @total = @order_details.inject(0) { |sum, item| sum + item.subtotal }
+    @order.postage = 800
+    @order.total_payment = @order.postage + @total.round.to_i
   end
 
   def update
+  end
+
+
+  private
+  def oeder_params
+    params.require(:order).permit(:customer_id, :postal_code, :address, :name, :payment_method, :total_payment, :postage, :status)
   end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,12 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
+
+  enum make_status: {
+    "製作不可":0, "製作待ち":1, "製作中":2, "製作完了":3
+  }
+
+  def subtotal
+    item.add_tax_price * amount
+  end
 end
