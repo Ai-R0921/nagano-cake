8a0d94c9a10b607a05541ed00f3945f15afa0cb4
Ai-R0921 <xoa1xo.0921@gmail.com>
2022-10-25 14:49:24 +0000

    [WIP]ジャンル検索機能実装中

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -8,11 +8,19 @@ class Public::ItemsController < ApplicationController
     else
       @items = @results.page(params[:page]).per(8)
     end
-  end
 
+    @genres = Genre.all
+    if params[:genre_id]
+      @genre = Genre.find(params[:id])
+      @items = @genre.items.page(params[:page]).per(8)
+    else
+      @items = Item.page(params[:page]).per(8)
+    end
+  end
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
+    @genres = Genre.all
   end
 
   private
